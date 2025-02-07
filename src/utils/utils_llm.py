64e97c75be8b85c2ff77dfe06de1429cc43364d2
skaddd <mateo.lebrun@telecomnancy.net>
2025-02-07 16:48:31 +0100

    [FEAT] Added Local LLMs functions

--- src/utils/utils_llm.py
+++ src/utils/utils_llm.py
@@ -1,16 +1,74 @@
+import logging
+from typing import Dict, Union
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.chat_models import ChatLlamaCpp
+from langchain_core.embeddings import Embeddings
+from langchain_core.language_models import BaseChatModel
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-from langchain_core.language_models import BaseChatModel
-from langchain_core.embeddings import Embeddings
-from typing import Dict, Union
-import logging
-
-
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def load_llm_model(llm_config: Dict[str, Union[str, int]]) -> BaseChatModel:
-    """Load Langchain chatmodel.
+def llamacpp_load_llm_model(llm_config: Dict[str, Union[str, int]]):
+    """Load Langchain chatmodel using llama.cpp.
+
+    Args:
+        llm_config (Dict[str, Union[str, int]]): config for the loading.
+
+    Returns:
+        BaseChatModel: CHatmodel selected.
+    """
+    try:
+        chat_model = ChatLlamaCpp(
+            model_path=llm_config["model_path"],
+            temperature=llm_config["temperature"],
+            max_tokens=llm_config["max_tokens"],
+            n_ctx=llm_config["n_ctx"],
+            top_p=llm_config["top_p"],
+            verbose=False,
+        )
+    except KeyError:
+        logger.info("Config file missing key parameters to load LLM")
+
+    logger.info("CPP model sucessfully loaded !")
+
+    return chat_model
+
+
+def llamacpp_load_embedding_model(embedding_config: Dict[str, str]):
+    """Load dense embedding model from HF.
+
+    Args:
+        embedding_config (Dict[str, str]): config for
+        the embedding model.
+
+    Returns:
+        Embeddings: Dense embedding object.
+    """
+    try:
+        # dense_embedding_model = SentenceTransformer(
+        #     embedding_config["model_name"]
+        # )
+        embedding_func = HuggingFaceEmbeddings(
+            model_name=embedding_config["model_name"]
+        )
+    except ValueError:
+        logger.info(f"Unable to load : {embedding_config['model_name']}")
+
+    logger.info("Embedding HF model sucessfully loaded !")
+
+    return embedding_func
+
+
+# As i struggled a lot with llama-cpp, openai were used at the begining.
+
+
+def openai_load_llm_model(
+    llm_config: Dict[str, Union[str, int]]
+) -> BaseChatModel:
+    """Load Langchain chatmodel using openai api.
 
     Args:
         llm_config (Dict[str, Union[str, int]]): config for the loading.
@@ -32,7 +90,7 @@ def load_llm_model(llm_config: Dict[str, Union[str, int]]) -> BaseChatModel:
     return chat_model
 
 
-def load_dense_embedding_model(
+def openai_load_dense_embedding_model(
     embedding_config: Dict[str, Union[str, int]]
 ) -> Embeddings:
     """Load dense embedding model.
@@ -55,3 +113,14 @@ def load_dense_embedding_model(
         logger.info("Config file missing key parameters to load Embedding")
 
     return dense_embedding_model
+
+
+if __name__ == "__main__":
+
+    from helpers import global_loading_configuration
+
+    config_dir = r"/home/mateo/projects/RAGCook/conf"
+
+    conf = global_loading_configuration(configuration_dir=config_dir)
+
+    llamacpp_load_embedding_model(conf["llama_embedding_config"])
